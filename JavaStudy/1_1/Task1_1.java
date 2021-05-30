f1df850cbf6a6fd64ce3c5408812d909bbbb7ad9
kusida <84723608+kusida@users.noreply.github.com>
2021-05-30 15:56:11 +0900

    データ型・文字列クラス
    1_1

---
+++ JavaStudy/1_1/Task1_1.java
@@ -0,0 +1,20 @@
+public class Task1_1{
+
+
+    public static void main(String[]args){
+        int hogeInt = 50;
+        String hogeString ="テスト";
+        //浮動小数点型　細かい数字を扱える
+        double hogeDouble = 0.2;
+        hogeInt =100;
+        hogeString ="合格";
+        
+        System.out.print(hogeInt);
+
+        //hogeStringを表示させる
+        System.out.print(hogeString);
+
+        System.out.print(hogeDouble);
+
+}
+}
